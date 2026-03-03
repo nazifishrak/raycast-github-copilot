33412f805b24d995cd2d6bdfedbd434d50cfdb80
Syed Nazif Ishrak <63534571+nazifishrak@users.noreply.github.com>
2026-03-02 19:29:05 -0800

    fixed bugs

--- src/index.tsx
+++ src/index.tsx
@@ -180,6 +180,8 @@ export default function Command() {
             });
             setIsLoading(false);
           },
+          undefined,
+          selectedModel,
         );
       } catch (error) {
         showToast({
